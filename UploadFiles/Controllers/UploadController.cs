eb2df667fe9042e8620bf78f9bfd496a6b7bcae9
Tchamde Djeumo Yves <tchamdeyves@gmail.com>
2022-07-11 11:55:03 +0200

    Creating of project

---
+++ UploadFiles/Controllers/UploadController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadFiles.Models;
+
+namespace UploadFiles.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult UploadFile()
+        {
+            var model = new FileModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(FileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.File != null)
+                {
+                    var pathFolder = "wwwroot/UploadFiles/";
+                    pathFolder += model.File.FileName;
+                    var pathServer = Path.Combine(_env.ContentRootPath, pathFolder);
+                    await model.File.CopyToAsync(new FileStream(pathServer, FileMode.Create));
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ UploadFiles/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFiles.Models
+{
+    public class FileModel
+    {
+        [Required(ErrorMessage = "You Must Choose a File"), Display(Name = "Choose File To Upload")]
+        public IFormFile File { get; set; }
+    }
+}
